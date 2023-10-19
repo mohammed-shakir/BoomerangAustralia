8ec9fd631a8f67e726ee5e6096acc73884097dc3
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-10-19 15:30:39 +0200

    refactor drafting

--- src/models/draft/Draft.java
+++ src/models/draft/Draft.java
@@ -5,22 +5,29 @@ import models.player.*;
 import models.cards.*;
 
 public class Draft implements Drafting {
+
+    private void lastToFirst(List<Player> allPlayers) {
+        List<Cards> lastPlayerCards = allPlayers.get(allPlayers.size() - 1).hand;
+        for (int i = allPlayers.size() - 1; i > 0; i--) {
+            allPlayers.get(i).hand = allPlayers.get(i - 1).hand;
+        }
+        allPlayers.get(0).hand = lastPlayerCards;
+    }
+
+    private void firstToLast(List<Player> allPlayers) {
+        List<Cards> firstPlayerCards = allPlayers.get(0).hand;
+        for (int i = 0; i < allPlayers.size() - 1; i++) {
+            allPlayers.get(i).hand = allPlayers.get(i + 1).hand;
+        }
+        allPlayers.get(allPlayers.size() - 1).hand = firstPlayerCards;
+    }
+
     @Override
     public void draft(List<Player> allPlayers) {
-        List<Cards> lastPlayerCards = allPlayers.get(allPlayers.size() - 1).hand;
-        List<Cards> firstPlayerCards = allPlayers.get(0).hand;
-
-        if (lastPlayerCards.size() > 1) {
-            for (int i = allPlayers.size() - 1; i > 0; i--) {
-                allPlayers.get(i).hand = allPlayers.get(i - 1).hand;
-            }
-
-            allPlayers.get(0).hand = lastPlayerCards;
+        if (allPlayers.get(allPlayers.size() - 1).hand.size() > 1) {
+            lastToFirst(allPlayers);
         } else {
-            for (int i = 0; i < allPlayers.size() - 1; i++) {
-                allPlayers.get(i).hand = allPlayers.get(i + 1).hand;
-            }
-            allPlayers.get(allPlayers.size() - 1).hand = firstPlayerCards;
+            firstToLast(allPlayers);
         }
     }
 }
